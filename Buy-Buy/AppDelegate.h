c13b3ce99f4e2c6c9883396120bca5b8ed166916
17710850724 <690373766@qq.com>
2016-08-17 11:50:21 +0800

    test

---
+++ Buy-Buy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Buy-Buy
+//
+//  Created by ma c on 16/8/17.
+//  Copyright © 2016年 LXq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Buy-Buy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Buy-Buy
+//
+//  Created by ma c on 16/8/17.
+//  Copyright © 2016年 LXq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
